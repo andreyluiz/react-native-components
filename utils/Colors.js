5e2bc9424c6ada9cf18835d26cbeadaad4fca823
Andrey Luiz <andreyluiz.developer@gmail.com>
2016-06-25 22:13:38 -0300

    Add missing file.

---
+++ utils/Colors.js
@@ -0,0 +1,15 @@
+export const Colors = {
+  transparent: 'rgba(0, 0, 0, 0)',
+  white: '#FFFFFF',
+  default_text: '#212121',
+  facebook_color: '#3B5998',
+  google_color: '#D73D32',
+  primary_color: '#005F00',
+  separator: '#B6B6B6',
+  primary_dark_color: '#004100',
+  primary_ripple_color: 'rgba(0, 29, 0, 0.4)',
+  light_ripple_color: 'rgba(173, 173, 173, 0.4)',
+  facebook_ripple_color: 'rgba(20, 48, 107, 0.4)',
+  google_ripple_color: 'rgba(139, 12, 2, 0.4)',
+  secondary_text_color: '#727272'
+}
